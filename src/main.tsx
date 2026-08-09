b121c66a443c3c94ce651fadb3bcf4b5170a430e
bayersophie <314743007+bayersophie@users.noreply.github.com>
2026-08-09 15:25:51 +0200

    Update navigation bar

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,42 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 
 import "./index.css";
 
 import App from "./App";
 import ProjectPage from "./pages/ProjectPage";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant",
+    });
+  }, [pathname]);
+
+  return null;
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Navigate to="/en" replace />} />
 
         <Route path="/en" element={<App language="en" />} />
+
         <Route path="/de" element={<App language="de" />} />
 
         <Route
